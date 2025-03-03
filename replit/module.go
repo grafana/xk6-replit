57bb519fa522474ecf354ab417a1cf4923c580b5
İnanç Gümüş <inanc.gumus@grafana.com>
2025-03-03 11:35:38 -0500

    Add run return a Sobek value

--- replit/module.go
+++ replit/module.go
@@ -1,22 +1,21 @@
 package replit
 
 import (
+	"github.com/grafana/sobek"
 	"go.k6.io/k6/js/modules"
 )
 
 type API struct {
 	Greeting string
-	Run      func(string) error
+	Run      func(string) (sobek.Value, error)
 }
 
 // NewModuleInstance returns a new [ModuleInstance].
 func (rm *RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
 	api := &API{
 		Greeting: "WELCOME TO REPLIT!",
-		Run: func(code string) error {
-			v, err := vu.Runtime().RunString(code)
-			_ = v
-			return err
+		Run: func(code string) (sobek.Value, error) {
+			return vu.Runtime().RunString(code)
 		},
 	}
 	return &ModuleInstance{
